eaa56574f6c414c0f8053d07ef0f4232290ec376
Steven Yung <hyiuen@fastmail.com>
2021-01-07 11:37:30 +0100

    WIP implement routing of notification to the right positional array

--- src/index.tsx
+++ src/index.tsx
@@ -20,21 +20,54 @@ let id = 1;
 function reducer(state, action) {
   switch (action.type) {
     case ADD:
-      return [...state, action.notification];
+      return {
+        ...state,
+        [action.notification.position]: [
+          ...state[action.notification.position],
+          action.notification,
+        ],
+      };
     case DISMISS:
-      return state.filter(({ id }) => id !== action.notification.id);
+      // we extract the position from the `id`
+      const [position] = action.notification.id.split('-');
+      return {
+        ...state,
+        [position]: state[position].filter(
+          ({ id }) => id !== action.notification.id,
+        ),
+      };
     default:
       return state;
   }
 }
 
-function useNotifications() {
-  const [notifications, dispatch] = useReducer(reducer, []);
+const positions = {
+  BOTTOM_RIGHT: 'bottomRight',
+  TOP: 'top',
+};
 
-  const notify = children => {
+function useNotifications() {
+  const [notifications, dispatch] = useReducer(reducer, {
+    [positions.BOTTOM_RIGHT]: [],
+    [positions.TOP]: [],
+  });
+
+  const notify = (children, config = { position: positions.BOTTOM_RIGHT }) => {
     const newId = id++;
 
-    dispatch({ type: ADD, notification: { id: newId, children } });
+    dispatch({
+      type: ADD,
+      notification: {
+        // `id` is compose of the position and a unique number
+        // so a first notification created on the top position will have the `id` = "top-1"
+        // then the second one on the bottom right will be `bottomRight-2`
+        // this system allow us to understand in which array is the notification using only the `id`
+        // check `DISMISS` action in the reducer
+        id: `${config.position}-${newId}`,
+        children,
+        position: config.position,
+      },
+    });
   };
 
   const dismiss = id => {
@@ -51,9 +84,9 @@ function NotifierContextProvider({
 }) {
   const { notifications, notify, dismiss } = useNotifications();
 
-  const activeNotifications = max
-    ? notifications.slice(Math.max(notifications.length - max, 0))
-    : notifications;
+  // const activeNotifications = max
+  //   ? notifications.slice(Math.max(notifications.length - max, 0))
+  //   : notifications;
 
   return (
     <NotifierContext.Provider
@@ -64,40 +97,46 @@ function NotifierContextProvider({
       {children}
 
       <div className="react-headless-notifier-fixed react-headless-notifier-top-0 react-headless-notifier-right-0 react-headless-notifier-left-0 react-headless-notifier-flex react-headless-notifier-flex-col-reverse react-headless-notifier-items-center">
-        {activeNotifications.map(({ id, children }) => (
-          <NotificationWrapper
-            position="top"
-            key={id}
-            duration={durationPerNotification}
-            onDismiss={() => dismiss(id)}
-          >
-            {children}
-          </NotificationWrapper>
-        ))}
+        {notifications[positions.TOP]
+          // .slice(Math.max(notifications[positions.TOP].length - max, 0))
+          .map(({ id, children }) => (
+            <NotificationWrapper
+              position="top"
+              key={id}
+              duration={durationPerNotification}
+              onDismiss={() => dismiss(id)}
+            >
+              {children}
+            </NotificationWrapper>
+          ))}
       </div>
 
       <div className="react-headless-notifier-fixed react-headless-notifier-bottom-0 react-headless-notifier-right-0 react-headless-notifier-m-8">
-        {activeNotifications.map(({ id, children }) => (
-          <NotificationWrapper
-            key={id}
-            duration={durationPerNotification}
-            onDismiss={() => dismiss(id)}
-            position="bottomRight"
-          >
-            {children}
-          </NotificationWrapper>
-        ))}
+        {notifications[positions.BOTTOM_RIGHT]
+          // .slice(
+          //   Math.max(notifications[positions.BOTTOM_RIGHT].length - max, 0),
+          // )
+          .map(({ id, children }) => (
+            <NotificationWrapper
+              key={id}
+              duration={durationPerNotification}
+              onDismiss={() => dismiss(id)}
+              position="bottomRight"
+            >
+              {children}
+            </NotificationWrapper>
+          ))}
       </div>
     </NotifierContext.Provider>
   );
 }
 
 const animations = {
-  bottomRight: {
+  [positions.BOTTOM_RIGHT]: {
     enter: 'react-headless-notifier-animate-enter-right',
     exit: 'react-headless-notifier-animate-exit-right',
   },
-  top: {
+  [positions.TOP]: {
     enter: 'react-headless-notifier-animate-enter-top',
     exit: 'react-headless-notifier-animate-exit-top',
   },
