8aa80129657a4783aa18b28df3258d8bc6c92e54
Steven Yung <hyiuen@fastmail.com>
2021-01-18 14:27:10 +0100

    add metatags

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -10,7 +10,56 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>React Headless Notifier</title>
+        {/* Primary Meta Tags */}
+        <title>
+          React Headless Notifier - Highly customizable notifications for React
+        </title>
+        <meta
+          name="title"
+          content="React Headless Notifier - Highly customizable notifications for React"
+        />
+        <meta
+          name="description"
+          content="React Headless Notifier is component library for building highly customizable notification system. This library is lightweight, and ultra-customizable, but do not render any markup or styles for you. This effectively means that React Headless Notifier is a headless UI library"
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://react-headless-notifier.recodable.io/"
+        />
+        <meta
+          property="og:title"
+          content="React Headless Notifier - Highly customizable notifications for React"
+        />
+        <meta
+          property="og:description"
+          content="React Headless Notifier is component library for building highly customizable notification system. This library is lightweight, and ultra-customizable, but do not render any markup or styles for you. This effectively means that React Headless Notifier is a headless UI library"
+        />
+        <meta
+          property="og:image"
+          content="https://react-headless-notifier.recodable.io/social.png"
+        />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://react-headless-notifier.recodable.io/"
+        />
+        <meta
+          property="twitter:title"
+          content="React Headless Notifier - Highly customizable notifications for React"
+        />
+        <meta
+          property="twitter:description"
+          content="React Headless Notifier is component library for building highly customizable notification system. This library is lightweight, and ultra-customizable, but do not render any markup or styles for you. This effectively means that React Headless Notifier is a headless UI library"
+        />
+        <meta
+          property="twitter:image"
+          content="https://react-headless-notifier.recodable.io/social.png"
+        />
       </Head>
 
       <MDXProvider>
